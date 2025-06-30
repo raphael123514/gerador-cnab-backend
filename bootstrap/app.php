507e7a179515b37bb04f3d36470d9a5b98425d7a
Raphael Fonseca <raphael.fonsec@hotmail.com>
2025-06-29 22:36:22 -0300

    feat: colocando sail e adicionando api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        api: __DIR__.'/../routes/api.php',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
